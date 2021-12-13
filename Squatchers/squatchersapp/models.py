f923c96c9cf67d955651274f29c9946be10cceb5
glennlivi9 <82484261+glennlivi9@users.noreply.github.com>
2021-12-13 12:57:51 -0700

    Co-authored-by: Zachary Barton <ZTBarton@users.noreply.github.com> Co-authored-by: Sam-Robertson <Sam-Robertson@users.noreply.github.com> Co-authored-by: TheJoshBaxter <TheJoshBaxter@users.noreply.github.com>

--- Squatchers/squatchersapp/models.py
+++ Squatchers/squatchersapp/models.py
@@ -20,10 +20,12 @@ class Reporter(models.Model):
         super(Reporter, self).save()
 
 class Sighting(models.Model):
-    reporter_id = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+    sight_title = models.CharField(max_length=100, blank=True)
     sight_latitude = models.CharField(max_length=10)
     sight_longitude = models.CharField(max_length=10)
     Time_date = models.DateTimeField(default=datetime.today)
+    time_zone = models.CharField(max_length=4, blank=True)
     Country = models.CharField(max_length=50)
     State = models.CharField(max_length=2)
     Image = models.ImageField(upload_to='photos', blank=True)
@@ -35,6 +37,7 @@ class Sighting(models.Model):
     def save(self):
         self.Country = self.Country.upper()
         self.State = self.State.upper()
+        self.time_zone = self.time_zone.upper()
         super(Sighting, self).save()
         
 
